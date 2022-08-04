40866f78fbdd51b41a211265999abf757b279a16
xiomara <xrodri98@gmail.com>
2022-08-04 13:48:25 -0400

    user routes finished

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const userModel = require("../models/userData.js");
 // const userModel = require("..models/userData.js");
 
 //routes
+//POST
 router.post("/signup", (request, response) => {
   const input = request.body;
 
@@ -33,6 +34,107 @@ router.post("/signup", (request, response) => {
       });
     }
   });
+});
+
+// LOGIN
+router.post("/login", (request, response) => {
+  const { username, password } = request.body;
+
+  let existingUser;
+
+  existingUser = userModel.findOne({ username: username }, (err, post) => {
+    if (err) {
+      console.log("ERROR " + err);
+      response.status(500).json({ message: "Problems logging in." });
+    } else {
+      console.log("User was successfully found.");
+      response.status(200).json({ message: "User successfully logged in." });
+    }
+  });
+});
+
+// GET ALL
+router.get("/all", (request, response) => {
+  userModel.find({}, "-password", (err, docs) => {
+    if (err) {
+      console.log("ERROR " + err);
+      response
+        .status(500)
+        .json({ message: "Problems when reading the information." });
+    } else {
+      // if everything is working
+      console.log("All the users were found.");
+      response.status(200).json({ docs });
+    }
+  });
+});
+
+// GET ONE
+router.get("/:userId", (request, response) => {
+  userModel.findOne(
+    {
+      _id: request.params.userId,
+    },
+    (err, post) => {
+      if (err) {
+        console.log("ERROR " + err);
+        response.status(500).json({ message: "Problems when reading user." });
+      } else {
+        console.log("User was successfully found.");
+        response.status(200).json(post);
+      }
+    }
+  );
+});
+
+//UPDATE
+router.put("/:userId", (request, response) => {
+  const input = request.body;
+
+  userModel.updateOne(
+    {
+      _id: request.params.userId,
+    },
+    {
+      username: input.username,
+      password: input.password,
+      // email: input.email,
+      profile: input.profile,
+    },
+    (err, result) => {
+      if (err) {
+        console.log("ERROR " + err);
+        response
+          .status(500)
+          .json({ message: "problems updating information." });
+      } else {
+        console.log("User successfully updated.");
+        response.status(200).json({ message: "User successfully updated." });
+      }
+    }
+  );
+});
+
+//DELETE
+router.delete("/:userId", (request, response) => {
+  userModel.deleteOne(
+    {
+      _id: request.params.userId,
+    },
+    (err) => {
+      if (err) {
+        console.log("ERROR " + err);
+        response
+          .status(500)
+          .json({ message: "Problems finding user to delete." });
+      } else {
+        console.log("User successfully deleted from mongoDB.");
+        response
+          .status(200)
+          .json({ message: "User successfully deleted from mongoDB." });
+      }
+    }
+  );
 });
 
 // used to make information available across the app
